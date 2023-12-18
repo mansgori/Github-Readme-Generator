2ca8f6ef5cc9a79ebb6b4f69e71f5cdefff4e4b9
mansgori <85434451+mansgori@users.noreply.github.com>
2023-12-18 15:07:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+  // Your other Next.js configuration options go here
+
+  // Add the exportPathMap configuration
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      // Add other pages as needed
+    };
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
